2a4b1471fca6f3761c789040e5842e2afbc724f2
Genevieve <genevieve.sublette@capitalone.com>
2017-03-05 11:48:50 -0800

    utilizes material-ui

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,11 +3,14 @@ import { connect } from 'react-redux';
 import Messages from './Messages';
 import VolunteerForm from './VolunteerForm.react';
 import Modal from 'react-modal';
-import RaisedButton from 'material-ui';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Card, CardActions, CardHeader} from 'material-ui/Card';
+// Needed for onTouchTap
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 var Home = React.createClass({
   getInitialState: function() {
-    return { modalIsOpen: false };
+    return { modalIsOpen: false};
   },
 
   openModal: function() {
@@ -18,18 +21,42 @@ var Home = React.createClass({
     this.setState({modalIsOpen: false});
   },
 
-  //TODO: the volunteer form should be rendered after a user selects a volunteer role to apply for.
+  renderCard: function () {
+    return (
+    <Card>
+      <CardHeader
+        title="Planned Parenthood"
+        subtitle="Abortion Clinic Escort"
+        actAsExpander={true}
+        showExpandableButton={true}
+      />
+      <CardActions>
+        <RaisedButton label="Apply" primary={true} onClick={this.openModal}/>
+      </CardActions>
+    </Card>
+    )
+  },
+
+  renderForm: function () {
+    const style = {
+      zIndex: 1000
+    }
+    return (
+      <Modal
+        isOpen={this.state.modalIsOpen}
+        onRequestClose={this.closeModal}
+        contentLabel="Modal" 
+        width={300}
+        style={style} >
+        <VolunteerForm iframe='iframe' src="https://form.jotform.com/70626599389172" height="600" width="650" onBlur={this.closeModal}/>
+      </Modal>
+    );
+  },
+
   render() {
     return (
       <div className="container-fluid">
-        <button onClick={this.openModal}>Apply to Volunteer</button>
-        <Modal
-          isOpen={this.state.modalIsOpen}
-          onRequestClose={this.closeModal}
-          contentLabel="Modal"
-        >
-          <VolunteerForm iframe='iframe' src="https://form.jotform.com/70626599389172" height="600" width="650" onBlur={this.closeModal}/>
-        </Modal>
+      {this.state.modalIsOpen ? this.renderForm() : this.renderCard() }
       </div>
     );
   }
@@ -37,7 +64,7 @@ var Home = React.createClass({
 
 const mapStateToProps = (state) => {
   return {
-    messages: state.messages
+    modalIsOpen: state.modalIsOpen
   };
 };
 
