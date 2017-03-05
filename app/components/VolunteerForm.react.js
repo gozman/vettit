2716d4ad61bf3ddc74a083c11b0498434182b64a
Alexxis Johnson <alexxis.johnson@capitalone.com>
2017-03-05 13:15:57 -0800

    Add styling to volunteer form

--- app/components/VolunteerForm.react.js
+++ app/components/VolunteerForm.react.js
@@ -3,7 +3,9 @@ import React from 'react';
 function VolunteerForm (props) {
   var Iframe = props.iframe;
   return (
-    <Iframe src={props.src} height={props.height} width={props.width}/>
+    <div className="form-wrapper">
+      <Iframe src={props.src} height={props.height} width={props.width}/>
+    </div>
   );
 }
 
